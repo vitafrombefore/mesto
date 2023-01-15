7fd1239d8aa3fa00346ad89924be1635882b6e6c
vitafrombefore <v.bednazh@gmail.com>
2023-01-16 00:28:22 +0300

    Внести правки после первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -4,29 +4,32 @@ const popupCloseButtonElement = popupElement.querySelector('.popup__close-button
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 let formElement = popupElement.querySelector('.popup__edit-form');
-let nameInput = formElement.querySelector('.popup__edit-name');
-let jobInput = formElement.querySelector('.popup__edit-job');
+let nameInput = formElement.querySelector('.popup__input_edit_name');
+let jobInput = formElement.querySelector('.popup__input_edit_job');
 
 const openPopup = function () {
-    popupElement.classList.add('popup_active');
+    popupElement.classList.add('popup_opened');
 }
 
 const closePopup = function () {
-    popupElement.classList.remove('popup_active');
+    popupElement.classList.remove('popup_opened');
 }
 
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileSubtitle.textContent;
+popupOpenButtonElement.addEventListener('click', function () {
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
+
+    openPopup();
+})
 
 function handleFormSubmit(evt) {
-    evt.preventDefault();
+        evt.preventDefault();
 
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
+        profileTitle.textContent = nameInput.value;
+        profileSubtitle.textContent = jobInput.value;
 
-    closePopup();
-}
+        closePopup();
+    }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 formElement.addEventListener('submit', handleFormSubmit);
