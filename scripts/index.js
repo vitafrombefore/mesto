82987d7c13d3e8e595d0a24cbe12fc1803ef77aa
vitafrombefore <v.bednazh@gmail.com>
2023-02-02 22:42:20 +0300

    Добавить второй попап

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,52 @@
-// edit profile popup
-const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-let profileTitle = document.querySelector('.profile__title');
-let profileSubtitle = document.querySelector('.profile__subtitle');
-let formElement = popupElement.querySelector('.popup__edit-form');
-let nameInput = formElement.querySelector('.popup__input_edit_name');
-let jobInput = formElement.querySelector('.popup__input_edit_job');
+// popups
+const allPopups = document.querySelectorAll('.popup');
+const editProfilePopup = document.querySelector('.popup__edit-profile');
+const addElementsPopup = document.querySelector('.popup__add-elements');
 
-const openPopup = function () {
-    popupElement.classList.add('popup_opened');
+// open popups
+const popupOpenEditButton = document.querySelector('.profile__edit-button');
+const popupOpenAddButton = document.querySelector('.profile__add-button');
+
+const openPopup = popup => {
+    popup.classList.add('popup_opened');
+};
+
+popupOpenEditButton.addEventListener('click', () => {
+    openPopup(editProfilePopup);
 
     nameInput.value = profileTitle.textContent;
     jobInput.value = profileSubtitle.textContent;
-}
+});
 
-const closePopup = function () {
-    popupElement.classList.remove('popup_opened');
-}
+popupOpenAddButton.addEventListener('click', () => openPopup(addElementsPopup));
 
-function handleFormSubmit(evt) {
+// close popups
+const popupCloseButton = document.querySelectorAll('.popup__close-button');
+
+const closePopup = popup => {
+    popup.classList.remove('popup_opened');
+};
+
+popupCloseButton.forEach(button => {
+    button.addEventListener('click', () => {
+        closePopup(button.closest('.popup'));
+    })
+});
+
+// edit profile popup
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+let profileEditForm = document.querySelector('.popup__form_edit');
+let nameInput = document.querySelector('.popup__input_edit_name');
+let jobInput = document.querySelector('.popup__input_edit_job');
+
+const handleFormSubmit = evt => {
     evt.preventDefault();
 
     profileTitle.textContent = nameInput.value;
     profileSubtitle.textContent = jobInput.value;
 
-    closePopup();
+    closePopup(editProfilePopup);
 }
 
-popupOpenButtonElement.addEventListener('click', openPopup);
-popupCloseButtonElement.addEventListener('click', closePopup);
-formElement.addEventListener('submit', handleFormSubmit);
+profileEditForm.addEventListener('submit', handleFormSubmit)
